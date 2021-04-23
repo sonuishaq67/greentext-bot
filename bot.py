6c5ae6e70e2c67d033e633a352f18bf0210dc9a1
Ishaq Shaik <sonuishaq67@gmail.com>
2021-04-23 12:08:01 +0530

    done with post

--- bot.py
+++ bot.py
@@ -4,35 +4,49 @@ import time
 import requests
 import random
 
+
 def auth():
     reddit = praw.Reddit(
-     client_id=config("client_id"),
-     client_secret=config("client_secret"),
-     password=config("password"),
-     username=config("username"),
-     user_agent=config("user_agent")
+        client_id=config("client_id"),
+        client_secret=config("client_secret"),
+        password=config("password"),
+        username=config("username"),
+        user_agent=config("user_agent"),
     )
     print("logged into reddit successfully")
     return reddit
 
 
-
 def extract(reddit):
-    with open('subreddits', 'r+') as subs:
-        substrs = subs.read()
+    with open("subreddits", "r+") as subs:
+        substrs = subs.readline().strip()
     try:
+        links_file = open(f"./img/posts", "w")
         print(substrs)
         subreddit = reddit.subreddit(substrs)
         for submission in subreddit.hot(limit=25):
-             img_url = submission.url
-             image_ext=str(img_url).split('/')[-1].split('.')[-1]
-             title=submission.title
-             print(image_ext)
-             r = requests.get(img_url, allow_redirects=True)
-             open(f'./img/{title}.{image_ext}', 'wb').write(r.content)
-        exit(0)
+            img_url = submission.url
+            image_ext = str(img_url).split("/")[-1].split(".")[-1]
+            title = submission.title
+            links_file.write(f'"{title}"\t{img_url}\n')
     except Exception as err:
         print(err)
-        time.sleep(20)
 
 
+def post(reddit):
+    with open("subreddits", "r+") as subs:
+        subs.readline()
+        substrs = subs.readline()
+        print(substrs)
+    try:
+        links_file = open(f"./img/posts", "r+")
+        subreddit=reddit.subreddit(substrs)
+        for line in links_file.readlines():
+            data = line.split("\t")
+            title = data[0].strip('"')
+            url = data[1]
+            print(f"posting {title}")
+            subreddit.submit(title=title,url=url)
+            print(f"posted {title}")
+    except Exception as err:
+        print(err)
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import bot
 from decouple import UndefinedValueError
 
 reddit = bot.auth()
-while reddit:
-    try:
-        bot.extract(reddit)
-    except UndefinedValueError as err:
-        print("Please create a .env file using the Readme.md")
+try:
+    bot.extract(reddit)
+    inp = input("Do you want to post now?")
+    if inp:
+        bot.post(reddit)
+except UndefinedValueError as err:
+    print("Please create a .env file using the Readme.md")
